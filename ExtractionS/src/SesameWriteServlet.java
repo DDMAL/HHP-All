2ccd7b81bf655459363c7aceff6212eaa58c46dc
Max <blarblar@github.com>
2015-12-28 17:39:11 -0500

    Changed dependancies, and wrote SesameWrite

--- ExtractionS/src/SesameWriteServlet.java
+++ ExtractionS/src/SesameWriteServlet.java
@@ -1,6 +1,8 @@
 
 
+import java.io.File;
 import java.io.IOException;
+
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +16,30 @@ import javax.servlet.http.HttpServletResponse;
 import org.jblas.util.Random;
 
 import com.google.gson.Gson;
+import info.aduna.iteration.Iterations;
+
+import org.openrdf.model.Literal;
+import org.openrdf.model.Model;
+import org.openrdf.model.Namespace;
+import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.model.impl.LinkedHashModel;
+import org.openrdf.model.impl.ValueFactoryImpl;
+import org.openrdf.model.vocabulary.FOAF;
+import org.openrdf.model.vocabulary.RDF;
+import org.openrdf.model.vocabulary.RDFS;
+import org.openrdf.model.vocabulary.XMLSchema;
+import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.RepositoryResult;
+import org.openrdf.repository.sail.SailRepository;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFHandlerException;
+import org.openrdf.rio.Rio;
+import org.openrdf.sail.memory.MemoryStore;
+import org.openrdf.sail.nativerdf.NativeStore;
 
 /**
  * Servlet implementation class SesameWriteServlet
@@ -37,7 +63,45 @@ public class SesameWriteServlet extends HttpServlet {
 		
 		// get the URL parameter
 		String data = request.getParameter("data");
-		System.out.println(data);
+
+
+		// sesame start up stuff
+		File dataDir = new File("./");
+		Repository rep = new SailRepository(new NativeStore(dataDir));
+		rep.initialize();
+
+		
+		try {
+			rep.initialize();
+		} catch (RepositoryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		RepositoryConnection conn = null;
+		try {
+			conn = rep.getConnection();
+		} catch (RepositoryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//
+				
+		String[] splitString = data.split("\n");
+		ValueFactory factory = ValueFactoryImpl.getInstance();
+		
+		for(int i = 0; i < splitString.length; i+=3) {
+			
+			URI a = factory.createURI(splitString[0]);
+			URI b = factory.createURI(splitString[1]);
+			Statement statement = factory.createStatement(a, b, factory.createLiteral(splitString[2]));
+			try {
+				conn.add(statement);
+			} catch (RepositoryException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		
 		//Now insert the data into Sesame
 		
@@ -53,6 +117,7 @@ public class SesameWriteServlet extends HttpServlet {
 		Gson gson = new Gson();
 		out.println(gson.toJson(jss));
 		out.close();
+		
 	}
 
 	/**
